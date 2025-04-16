3a9d62a959c174dfadc966d1cecffd57d801ee58
Andre Baltieri <andrebaltieri@hotmail.com>
2025-04-16 08:42:38 -0300

    Deploying package to Nuget

---
+++ Extensions/MediatorExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Balta.Mediator.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Balta.Mediator.Extensions;
+
+public static class MediatorExtension
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddTransient<IMediator, Mediator>();
+        
+        var handlerInterfaceType = typeof(IHandler<,>);
+
+        foreach (var assembly in assemblies)
+        {
+            var handlers = assembly
+                .GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                .Where(ti => ti.Interface.IsGenericType && ti.Interface.GetGenericTypeDefinition() == handlerInterfaceType);
+
+            foreach (var handler in handlers) 
+                services.AddTransient(handler.Interface, handler.Type);
+        }
+
+        return services;
+    }
+}
---
+++ Mediator.cs
@@ -0,0 +1,27 @@
+﻿using Balta.Mediator.Abstractions;
+
+namespace Balta.Mediator;
+
+public class Mediator(IServiceProvider serviceProvider) : IMediator
+{
+    public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request,
+        CancellationToken cancellationToken = default)
+    {
+        var requestType = request.GetType();
+        var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+
+        var handlerObj = serviceProvider.GetService(handlerType);
+        if (handlerObj == null)
+            throw new InvalidOperationException($"Handler not found for {requestType.Name}");
+
+        var method = handlerType.GetMethod("HandleAsync");
+        if (method == null)
+            throw new InvalidOperationException($"HandleAsync method not found in {handlerType.Name}");
+
+        if (method.Invoke(handlerObj, [request, cancellationToken]) is not Task<TResponse> task)
+            throw new InvalidOperationException(
+                $"Handler {handlerObj.GetType().Name} returned null or an incompatible type.");
+
+        return await task;
+    }
+}
